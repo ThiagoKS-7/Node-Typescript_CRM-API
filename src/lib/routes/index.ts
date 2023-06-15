a2efd6a58cd872df8b17e300189da97a3b46938e
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-06-15 17:40:54 -0300

    Configures CORS

--- src/lib/routes/index.ts
+++ src/lib/routes/index.ts
@@ -1,5 +1,9 @@
 module.exports = function (fastify: any, opts: any, done: Function) {
   fastify.register(require("@fastify/auth")).after(() => {
+    fastify.register(require("@fastify/cors"), { 
+      origin:'*',
+      methods:['POST', 'GET', 'PUT', 'PATCH', 'DELETE'],
+    })
     fastify.register(require("../modules/agent"), { prefix: "/agent" });
     fastify.register(require("../modules/client"), { prefix: "/agent" });
   });
