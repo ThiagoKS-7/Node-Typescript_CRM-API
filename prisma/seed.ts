f7ac2c40f1500cffddba1e53e0b296c6fc40b163
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-06-15 18:20:31 -0300

    Adjusts seed for deploy

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,23 +4,14 @@ import { hash } from "bcrypt";
 async function main() {
   const pwd = process.env.SEEDPWD;
   const passwordHash = await hash(pwd as string, 8);
-  const admin = await prisma.agent.findFirst({
-    where: {
+  await prisma.agent.create({
+    data: {
       name: "admin",
+      password: passwordHash,
+      clients: [],
     },
   });
-  if (!admin) {
-    await prisma.agent.create({
-      data: {
-        name: "admin",
-        password: passwordHash,
-        clients: [],
-      },
-    });
-    console.log("[INFO] Admin successfully created!");
-  } else {
-    console.log("[INFO] Admin already exists!");
-  }
+  console.log("[INFO] Admin successfully created!");
 }
 
 main();
